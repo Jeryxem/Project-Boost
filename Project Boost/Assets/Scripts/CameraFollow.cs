ba5167ef18b89166e87fd7efc261d867078e31fd
Jeryxem <43191142+Jeryxem@users.noreply.github.com>
2020-09-15 17:43:50 +0800

    Push before adding particle system effect pack

---
+++ Project Boost/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform cameraTarget;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        if (cameraTarget == null)
+            return;
+
+        Vector3 desiredPosition = cameraTarget.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(cameraTarget);
+    }
+}
